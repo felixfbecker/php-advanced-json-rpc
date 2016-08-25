8c569151ff8d3a62423bb9129229467f2e1d4855
Felix Becker <felix.b@outlook.com>
2016-08-25 12:38:42 +0200

    Add Message::parse()

--- lib/Message.php
+++ lib/Message.php
@@ -15,6 +15,31 @@ abstract class Message
      */
     public $jsonrpc;
 
+    /**
+     * Returns the appropiate Message subclass
+     *
+     * @param string $msg
+     * @return Message
+     */
+    public static function parse(string $msg): Message
+    {
+        $decoded = json_decode($this->buffer);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new ResponseError(json_last_error_msg(), ErrorCode::PARSE_ERROR);
+        }
+        if (Notification::isNotification($decoded)) {
+            $obj = new Notification;
+        } else if (Request::isRequest($decoded)) {
+            $obj = new Request;
+        } else if (Request::isResponse($decoded)) {
+            $obj = new Response;
+        }
+        foreach (get_object_vars($decoded) as $key => $value) {
+            $obj->$key = $value;
+        }
+        return $obj;
+    }
+
     public function __toString(): string
     {
         return json_encode($this);
